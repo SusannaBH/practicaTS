12610f0a1bad58e13af92535c8e7d9251bb1afd0
SusannaBH <susannabergaz@hotmail.com>
2023-10-12 18:49:51 +0200

    Co-authored-by: Manuel <ShadeVI@users.noreply.github.com>

--- ts/bombilla.ts
+++ ts/bombilla.ts
@@ -1,23 +1,42 @@
+//VARIABLES
+let on = false; // Inicialmente, la bombilla está apagada
+
 //TODOS LOS ELEMENTOS
-const botton = document.getElementById('switch') as HTMLImageElement;
-const bombilla = document.getElementById('lightbulb') as HTMLImageElement;
-
-//FUNCION 
-
+const bottonEl = document.getElementById('switch') as HTMLImageElement;
+const [bombilla_1_El, bombilla_2_El] = document.getElementsByClassName('bombilla') as HTMLCollectionOf<HTMLImageElement>;
+//const bombilla_1_El = bombillasEl[0] as HTMLImageElement;
+//const bombilla_2_El = bombillasEl[1] as HTMLImageElement;
+const lightEl = document.getElementById('barraLuminosidad') as HTMLInputElement;
+const rotateEl = document.getElementById('barraRotacion') as HTMLInputElement;
+const sizeEl = document.getElementById('barraTamaño') as HTMLInputElement;
 
 //TODOS LOS EVENTOS
-botton.addEventListener('click',()=>{
-    let on = true;
-    if (on) {
-        //Si está encendido, restaurar la imagen inicial
-        botton.src = "/IMAGES/bon.jpg";
-        bombilla.src = "/IMAGES/on.jpg";
+bottonEl.addEventListener('click', () => {
+    if (on == true) {
+        // Si la bombilla está encendida, apagarla
+        bottonEl.src = "/IMAGES/boff.jpg";
+        bombilla_1_El.src = "/IMAGES/off.jpg";   
     } else {
-        //Si está apagado, cambiar a la segunda imagen
-        botton.src = "/IMAGES/boff.jpg";
-        bombilla.src = "/IMAGES/off.jpg";
+        // Si la bombilla está apagada, encenderla
+        bottonEl.src = "/IMAGES/bon.jpg";
+        bombilla_1_El.src = "/IMAGES/on.jpg";
     }
-    //Alternar el estado
+    // Alternar el estado de la bombilla
     on = !on;
 });
-
+rotateEl.addEventListener('change', () => {
+    const value = rotateEl.value;
+    //console.log(value); //Probar salida de valor
+    bombilla_1_El.style.transform = `rotate(${value}deg)`;
+    bombilla_2_El.style.transform = `rotate(${value}deg)`;
+});
+sizeEl.addEventListener('change', () => {
+    const value = sizeEl.value;
+    bombilla_1_El.style.width = `${value}%`; // Mantener la relación de aspecto
+    bombilla_2_El.style.width = `${value}%`;
+});
+lightEl.addEventListener('change', () => {
+    if(!on) return;
+    const value = lightEl.value;
+    bombilla_1_El.style.opacity =  value;
+});
